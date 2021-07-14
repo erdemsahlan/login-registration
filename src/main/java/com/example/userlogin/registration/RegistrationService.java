2d0a0f60038d856ffe5739eb6572e7e3a578ad7e
Erdem Şahlan <sahlanerdem@gmail.comgit config --global user.email sahlanerdem@gmail.comgit config --global user.email sahlanerdem@gmail.com>
2021-07-14 22:15:33 +0300

    first-upload

---
+++ src/main/java/com/example/userlogin/registration/RegistrationService.java
@@ -0,0 +1,32 @@
+package com.example.userlogin.registration;
+
+import com.example.userlogin.appuser.AppUser;
+import com.example.userlogin.appuser.AppUserRole;
+import com.example.userlogin.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    private final EmailValidator emailValidator;
+    private final AppUserService appUserService;
+
+    public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                       request.getFirstName(),
+                       request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
+    }
+}
